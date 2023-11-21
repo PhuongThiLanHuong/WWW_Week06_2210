46f968c47fa69ac74cae3a972427bca2a421626e
LanHuong1507 <20057651.huong@student.iuh.edu.vn>
2023-11-21 13:42:35 +0700

    create database successfully

---
+++ src/main/java/vn/edu/iuh/fit/www_week06_2210/WwwWeek062210Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week06_2210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwWeek062210Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwWeek062210Application.class, args);
+    }
+
+}
